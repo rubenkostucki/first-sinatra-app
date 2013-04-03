d92a0ddd56c58f4e6077c0df14724c6b8cbf8d2a
Ruben Kostucki <rubenkostucki@gmail.com>
2013-04-03 19:45:44 +0100

    data mapper installed

--- sinatra-app.rb
+++ sinatra-app.rb
@@ -8,6 +8,19 @@ require 'data_mapper'
 
 DataMapper.setup(:default, "postgres://localhost/user")
 
+class User
+  include DataMapper::Resource
+
+  property :id,         Serial    # An auto-increment integer key
+  property :first_name,      String    # A varchar type string, for short strings
+  property :last_name,       String      # A text block, for longer string data.
+  property :special_skill,    Text # A text block, for longer string data.
+  property :created_at, DateTime  # A DateTime, for any date you might like.
+end
+
+DataMapper.finalize
+User.auto_upgrade!
+
 # get '/' do
 #   @user = [User.new("Aouled", "Miguil"), User.new("Ruben", "Kostucki")]
 #   @user[0].special_skill = "Sleeping while Sleeping"
@@ -15,7 +28,25 @@ DataMapper.setup(:default, "postgres://localhost/user")
 #   erb :user_profiles
 # end
 
+
 get '/user-form' do
   # This will be your default route
   erb :form
 end
+
+post '/profiles' do
+  @user = User.create(
+  :first_name      => "Phil",
+  :last_name       => "Jones",
+  :created_at => Time.now
+)
+  # erb :form
+end
+
+User.auto_migrate!
+
+get '/profiles' do
+  @user = User.find(params[:first_name])
+  erb :user_profiles
+end
+
