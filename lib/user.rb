825dd99b4c34bf52f8076550f8b34edd839fc055
Ruben Kostucki <rubenkostucki@gmail.com>
2013-04-05 13:49:41 +0100

    progressed with a delete functionality and currently trying to change the user-form mechanism

--- lib/user.rb
+++ lib/user.rb
@@ -4,6 +4,21 @@ class User
   property :id,         Serial    # An auto-increment integer key
   property :first_name,      String    # A varchar type string, for short strings
   property :last_name,       String      # A text block, for longer string data.
-  property :special_skill,    Text # A text block, for longer string data.
-  property :created_at, DateTime  # A DateTime, for any date you might like.
+  property :special_skill,   Text # A text block, for longer string data.
+  property :created_at,      DateTime  # A DateTime, for any date you might like.
+
+  has n, :comments
+end
+
+
+
+
+class Comment
+  include DataMapper::Resource
+
+  property :id,         Serial
+  property :text,       Text
+  property :created_at, DateTime
+
+  belongs_to :user
 end
--- sinatra-app.rb
+++ sinatra-app.rb
@@ -13,35 +13,44 @@ DataMapper.finalize
 DataMapper.auto_upgrade!
 
   get '/user-form' do
+
     erb :form
   end
 
 
-  post '/users' do
+  post '/user-form' do
     @user = User.create(
       :first_name      => params["firstname"],
       :last_name       => params["lastname"],
       :special_skill   => params["specialskill"],
-      :created_at =>   Time.now
-
-    )
-    "Form Sent"
+      :created_at =>   Time.now)
+      redirect '/user/#{@user.id}'
   end
+
+  get '/user/:id' do
+    @user = User.get(params[:id])
+    erb :user
+  end
+
 
   get '/users' do
     @users = User.all
 
-    erb :user_profile
+    erb :all_users
 
   end
 
-  get '/edit-user/:id' do
+  get '/user-edit/:id' do
     @user = User.get(params[:id])
+    if @user.nil?
+      not_found
+    end
     erb :edit_user
   end
 
-  put '/edit-user/:id' do
-    @user = User.update(
+  put '/user-edit/:id' do
+    @user = User.get(params[:id])
+    @user.update(
       :first_name      => params["firstname"],
       :last_name       => params["lastname"],
       :special_skill   => params["specialskill"],
@@ -50,3 +59,16 @@ DataMapper.auto_upgrade!
     )
     "User updated"
   end
+
+  delete '/user-edit/:id' do
+    @user = User.get(params[:id])
+    @user.destroy
+
+    "User deleted"
+  end
+
+  # not_found do
+  #   status 404
+  #   "Oopsy Daisy ... this user has been deleted by"
+  #   # erb :not_found
+  # end
